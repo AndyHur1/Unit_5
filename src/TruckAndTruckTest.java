127a3323990714a35094adfe64a84cd089c6314d
AndyHur1 <hurby@ssfs.org>
2020-01-30 11:10:34 -0500

    Working on truck and trucktest

---
+++ src/TruckAndTruckTest.java
@@ -0,0 +1,19 @@
+public class TruckAndTruckTest {
+    private String truckID;
+    private int odometer;
+    private double mpg;
+    private double fuel;
+    private final double capacity = 20.0;
+    public static double totalFuel;
+
+public String getTruckID(){
+    return truckID;
+}
+public int getOdometer(){
+    return odometer;
+}
+public double getMpg(){
+    return mpg;
+}
+public
+}
