0e16899d5427b3018921e1cc4417aabbb8d807f4
AndyHur1 <hurby@ssfs.org>
2020-01-31 11:20:19 -0500

    Working on truck and trucktest

--- src/TruckAndTruckTest.java
+++ src/TruckAndTruckTest.java
@@ -7,13 +7,50 @@ public class TruckAndTruckTest {
     public static double totalFuel;
 
 public String getTruckID(){
+
     return truckID;
 }
 public int getOdometer(){
+
     return odometer;
 }
 public double getMpg(){
     return mpg;
 }
-public
+public double getFuel(){
+    return fuel;
+}
+public boolean enoughFuel(){
+    boolean enough = false;
+    if (fuel*mpg<odometer){
+        enough=false;
+    }else if (fuel*mpg>=odometer){
+        enough=false;
+    }
+return enough;
+}
+public String drive(){
+    String drives = "";
+    if (fuel*mpg>=odometer){
+        fuel=fuel-(odometer/mpg);
+        drives="Success";
+    }else{
+        drives="Error";
+    }
+    return drives;
+}
+public void fill(){
+    fuel+=(odometer-mpg*fuel)/mpg;
+}
+public String fill(double gal){
+    String result = "";
+    if (fuel<=capacity){
+        result = "Success";
+    }else {
+        result = "Error";
+    }
+    return result;
+
+}
+
 }
